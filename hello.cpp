c9e2abfe7319cd6fee7a1fd489cadc7eb0e8a3bf
Piyush <piyush2k13@gmail.com>
2019-02-24 19:31:05 +0530

    added vscode files

--- hello.cpp
+++ hello.cpp
@@ -4,5 +4,10 @@ using namespace std;
 
 
 int main(int argc, char** argv) {
-    cout<<"Hello World !!!"<<endl;
+    cout << "Hello World !!!" << endl;
+    char mychar = 'a';
+    cout << mychar << endl;
+    const char16_t message[] = u"你好，世界\n";
+    cout << " Chinese Hello World " << message << endl;
+    cout << sizeof(message)/sizeof(char16_t) << "\n";
 }
