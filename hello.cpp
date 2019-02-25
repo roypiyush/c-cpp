645720a0e0299707d0ffa2c55154f9c910ae32b7
Piyush <piyush2k13@gmail.com>
2019-02-25 22:36:06 +0530

    added thread demo

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 #include <climits>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
 
 void goto_demo();
 long factorial (const long &a);
+void readFile();
+
+void overload(int a) {
+    cout << a << endl;
+}
+
+void overload(int a, int b) {
+    cout << a + b << endl;
+}
+
+void overload(int a, int &b) {
+    cout << a + b << endl;
+}
+
 
 int main(int argc, char** argv) {
     cout << "Hello World !!!" << endl;
@@ -19,8 +34,11 @@ int main(int argc, char** argv) {
     string mystring = "Simple \a String";
     cout << mystring << endl;
     goto_demo();
-    cout << factorial(5) << endl;
-
+    cout << "Factorial " << factorial(5) << endl;
+    overload(1);
+    overload(1, 2);
+    overload(1, 2);
+    readFile();
 }
 
 void goto_demo() {
@@ -33,8 +51,21 @@ void goto_demo() {
 }
 
 long factorial (const long &a) {
-  if (a > 1)
-   return (a * factorial (a-1));
-  else
-   return 1;
+    if (a > 1)
+        return (a * factorial (a-1));
+    else
+        return 1;
+}
+
+void readFile() {
+    string line;
+    ifstream myfile ("/var/log/kern.log");
+    if (myfile.is_open()) {
+        while ( getline (myfile,line) ) {
+            cout << line << '\n';
+        }
+        myfile.close();
+    } else {
+        cout<< "Coudn't open file" << endl;
+    }
 }
